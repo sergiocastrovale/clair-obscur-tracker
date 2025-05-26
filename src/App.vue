d5e3b4ff831b6eec5fa597b5de6ae704fc25a1db
Sérgio Castro <sergio.castro.vale@gmail.com>
2025-05-26 17:01:35 +0100

    chore: add Vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { Analytics } from '@vercel/analytics/vue';
 import Header from './components/Header.vue';
 </script>
 
 <template>
   <div class="h-full overflow-hidden relative">
+    <Analytics />
     <Header />
     <router-view />
   </div>
